8dda314460be5cebe4a8814cd93352ea3cdbface
MyKN <muhammedyasinkaynar@gmail.com>
2024-06-10 14:36:24 +0300

    Commit message describing the changes

--- build/costmap_erase_plugin/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/muhammedyasin/catkin_ws/src/costmap_erase_plugin
-# Build directory: /home/muhammedyasin/catkin_ws/build/costmap_erase_plugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
