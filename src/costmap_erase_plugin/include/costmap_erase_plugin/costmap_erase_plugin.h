e007e317aa62f80c16143f513795cd82dda01cc6
MyKN <muhammedyasinkaynar@gmail.com>
2024-07-01 15:31:22 +0300

    Code structure was changed

--- src/costmap_erase_plugin/include/costmap_erase_plugin/costmap_erase_plugin.h
+++ src/costmap_erase_plugin/include/costmap_erase_plugin/costmap_erase_plugin.h
@@ -1,47 +1,54 @@
-#ifndef COSTMAP_ERASE_PLUGIN_H
-#define COSTMAP_ERASE_PLUGIN_H
+#ifndef COSTMAP_ERASE_PLUGIN_H_
+#define COSTMAP_ERASE_PLUGIN_H_
 
-#include <costmap_2d/layer.h>
-#include <costmap_2d/layered_costmap.h>
-#include <costmap_2d/costmap_layer.h>
 #include <costmap_2d/obstacle_layer.h>
-#include <ros/ros.h>
+#include <costmap_2d/inflation_layer.h>
+#include <pluginlib/class_list_macros.h>
+
 #include <tf/transform_listener.h>
-#include <nav_msgs/Odometry.h>
+
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+
 #include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+#include <laser_geometry/laser_geometry.h>
+#include <set>
 #include <map>
-#include <utility>
+#include <vector>
 
-namespace costmap_2d {
+namespace costmap_2d
+{
+    class CostmapErasePlugin : public costmap_2d::ObstacleLayer
+    {
+    public:
+        CostmapErasePlugin();
+        virtual ~CostmapErasePlugin();
 
-class CostmapErasePlugin : public ObstacleLayer {
-public:
-    CostmapErasePlugin();
-    virtual void onInitialize();
-    virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x, double* max_y);
-    virtual void updateCosts(Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
+        virtual void onInitialize();
+        virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x, double* max_y);
+        virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
 
-private:
-    void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
-    void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
-    void checkObjectPersistence();
+    private:
+        void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+        void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
+        void initializeStaticCells();
 
-    ros::Subscriber odom_sub_;
-    ros::Subscriber laser_scan_sub_;
-    tf::TransformListener tf_listener_;
+        std::vector<std::pair<unsigned int, unsigned int>> static_cells_;
+        std::map<std::pair<unsigned int, unsigned int>, ros::Time> dynamic_cells_;
 
-    bool clear_obstacles_;
+        double robot_x_, robot_y_;
+        laser_geometry::LaserProjection projector_;
 
-    double erase_radius_;
-    double robot_x_;
-    double robot_y_;
-    double tolerance_;
-    double object_persistence_time_;
-    
-    
-    std::map<std::pair<unsigned int, unsigned int>, ros::Time> observed_objects;
-};
+        tf2_ros::Buffer tf_buffer_;
+        tf2_ros::TransformListener tf_listener_2;
+        tf::TransformListener tf_listener_1;
 
-} // end namespace costmap_2d
+        ros::Subscriber laser_scan_sub_;
+        ros::Subscriber odom_sub_;
 
-#endif // COSTMAP_ERASE_PLUGIN_H
+        costmap_2d::InflationLayer* inflation_layer_;
+    };
+}
+
+#endif // COSTMAP_ERASE_PLUGIN_H_
--- src/costmap_erase_plugin/src/costmap_erase_plugin.cpp
+++ src/costmap_erase_plugin/src/costmap_erase_plugin.cpp
@@ -1,97 +1,130 @@
 #include <costmap_erase_plugin/costmap_erase_plugin.h>
 #include <pluginlib/class_list_macros.h>
 #include <costmap_2d/costmap_2d.h>
-#include <costmap_2d/obstacle_layer.h>
-#include <nav_msgs/Odometry.h>
+#include <costmap_2d/inflation_layer.h>
 #include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
 #include <laser_geometry/laser_geometry.h>
-
 #include <ros/ros.h>
-#include <map>
-#include <cmath>
+#include <set>
+#include <vector>
+#include <algorithm>
 
 PLUGINLIB_EXPORT_CLASS(costmap_2d::CostmapErasePlugin, costmap_2d::Layer)
 
 namespace costmap_2d {
-  CostmapErasePlugin::CostmapErasePlugin() : clear_obstacles_(true), erase_radius_(2.0), robot_x_(0.0), robot_y_(0.0), tolerance_(1.0), object_persistence_time_(2.0) {
-    
+
+CostmapErasePlugin::CostmapErasePlugin() : tf_listener_2(tf_buffer_) {
     ros::NodeHandle nh("~/" + name_);
-
-    odom_sub_ = nh.subscribe("/odom", 1, &CostmapErasePlugin::odomCallback, this);
     laser_scan_sub_ = nh.subscribe("/scan", 1, &CostmapErasePlugin::laserScanCallback, this);
-  }
+    odom_sub_ = nh.subscribe("/odom", 1, &CostmapErasePlugin::odomCallback, this);
+}
 
-  void CostmapErasePlugin::onInitialize() {
+void CostmapErasePlugin::onInitialize() {
     ObstacleLayer::onInitialize();
-  }
+    //ROS_INFO("CostmapErasePlugin::onInitialize started");
 
-  void CostmapErasePlugin::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x, double* max_y) {
-    ObstacleLayer::updateBounds(robot_x, robot_y, robot_yaw, min_x, min_y, max_x, max_y); 
-  }
+    // Statik hücreleri başlat
+    initializeStaticCells();
 
-  void CostmapErasePlugin::updateCosts(Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j) {
-      auto now = ros::Time::now();
-      //ROS_INFO("Observed objects map size before UPDATECOST 1: %lu", observed_objects.size());
+    // Inflation layer'ı başlat
+    inflation_layer_ = new costmap_2d::InflationLayer();
+    inflation_layer_->initialize(layered_costmap_, name_ + "_inflation", &tf_buffer_);
 
-      for (auto it = observed_objects.begin(); it != observed_objects.end();) {
-          unsigned int mx, my;
-
-          mx = it->first.first;
-          my = it->first.second;
-          
-          double wx, wy;
-          master_grid.mapToWorld(mx, my, wx, wy);
-          double distance = std::hypot(wx - robot_x_, wy - robot_y_);
-
-        // Nesne 2 metre çapı dışında ve gözlemlenme süresi dolunca sil
-        // Object was stayed out of radius, update it after persistence time
-          if (distance > erase_radius_ && (now - it->second).toSec() > object_persistence_time_) {
-              master_grid.setCost(mx, my, costmap_2d::FREE_SPACE);
-              it = observed_objects.erase(it);
-          } else {
-            // Nesne 2 metre çapı içinde ise engel olarak tut
-            // If object stays in 2 metres radius of robot, define it as Obstacle
-              master_grid.setCost(mx, my, costmap_2d::LETHAL_OBSTACLE);
-              ++it;
-          }
-      }
-
-      //ROS_INFO("Observed objects map size after UPDATECOST 2: %lu", observed_objects.size());
-
-      // ROS_INFO was wrote for observing size of map vector 
-      // ROS_INFO map vektörünün durumunu kontrol etmek için yazıldı
-  }
+    //ROS_INFO("CostmapErasePlugin::onInitialize finished");
+}
 
 
-  void CostmapErasePlugin::laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
-    laser_geometry::LaserProjection projector_;
+
+
+void CostmapErasePlugin::initializeStaticCells() {
+    unsigned int size_x = layered_costmap_->getCostmap()->getSizeInCellsX();
+    unsigned int size_y = layered_costmap_->getCostmap()->getSizeInCellsY();
+    std::cout << "Static layer size: " << size_x << " x " << size_y << std::endl;
+
+    if (size_x > 0 && size_y > 0) {
+        static_cells_.clear();
+        for (unsigned int i = 0; i < size_x; ++i) {
+            for (unsigned int j = 0; j < size_y; ++j) {
+                if (layered_costmap_->getCostmap()->getCost(i, j) == costmap_2d::LETHAL_OBSTACLE) {
+                    static_cells_.push_back(std::make_pair(i, j));
+                }
+            }
+        }
+        //std::cout << "Static cells initialized with size: " << static_cells_.size() << std::endl;
+    } else {
+        //std::cout << "Static layer size is zero, initialization skipped." << std::endl;
+    }
+}
+
+void CostmapErasePlugin::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x, double* max_y) {
+    ObstacleLayer::updateBounds(robot_x, robot_y, robot_yaw, min_x, min_y, max_x, max_y);
+    inflation_layer_->updateBounds(robot_x, robot_y, robot_yaw, min_x, min_y, max_x, max_y);
+}
+
+void CostmapErasePlugin::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j) {
+    // Statik hücreleri koru
+    for (const auto& cell : static_cells_) {
+        master_grid.setCost(cell.first, cell.second, costmap_2d::LETHAL_OBSTACLE);
+    }
+
+    // Dinamik hücreleri güncelle ve görünmeyenleri sil
+    ros::Time current_time = ros::Time::now();
+    for (auto it = dynamic_cells_.begin(); it != dynamic_cells_.end(); ) {
+        if ((current_time - it->second).toSec() > 2.0) { // 2 saniyeden uzun süredir görünmüyorsa
+            master_grid.setCost(it->first.first, it->first.second, costmap_2d::FREE_SPACE); // Hücreyi temizle
+            it = dynamic_cells_.erase(it); // Dinamik hücre listesinden sil
+        } else {
+            master_grid.setCost(it->first.first, it->first.second, costmap_2d::LETHAL_OBSTACLE);
+            ++it;
+        }
+    }
+
+    // Inflation layer'ı güncelle
+    inflation_layer_->updateCosts(master_grid, min_i, min_j, max_i, max_j);
+}
+
+void CostmapErasePlugin::laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
     sensor_msgs::PointCloud cloud;
 
     try {
-        tf_listener_.waitForTransform("map", scan->header.frame_id, scan->header.stamp, ros::Duration(1.0));
-        projector_.transformLaserScanToPointCloud("map", *scan, cloud, tf_listener_);
-    } 
-    catch (tf::TransformException &ex) 
-    {
-        ROS_ERROR("Error transforming laser scan into the map frame: %s", ex.what());
+        tf_listener_1.waitForTransform(layered_costmap_->getGlobalFrameID(), scan->header.frame_id, scan->header.stamp, ros::Duration(1.0));
+        projector_.transformLaserScanToPointCloud(layered_costmap_->getGlobalFrameID(), *scan, cloud, tf_listener_1);
+    } catch (tf::TransformException &ex) {
+        //ROS_ERROR("Error transforming laser scan into the global frame: %s", ex.what());
         return;
     }
 
-    // Tanımlanan nesneyi obstacle ya da değil olarak güncelle
-    // Update Object as obstacle or not
-
+    // Dinamik objeleri güncelle
+    //ROS_INFO("Laser scan callback started. Observed objects size before clearing: %lu", dynamic_cells_.size());
+    
     for (const auto& point : cloud.points) {
-        double ox = point.x;
-        double oy = point.y;
         unsigned int mx, my;
-        if (layered_costmap_->getCostmap()->worldToMap(ox, oy, mx, my)) {
-            observed_objects[{mx, my}] = ros::Time::now();
+        if (layered_costmap_->getCostmap()->worldToMap(point.x, point.y, mx, my)) {
+            std::pair<unsigned int, unsigned int> cell_id(mx, my);
+            if (std::find(static_cells_.begin(), static_cells_.end(), cell_id) == static_cells_.end()) {
+                dynamic_cells_[cell_id] = ros::Time::now();
+            }
         }
     }
-  }
 
-  void CostmapErasePlugin::odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+    //ROS_INFO("Laser scan callback executed. Observed objects size after updating: %lu", dynamic_cells_.size());
+}
+
+
+
+
+void CostmapErasePlugin::odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+    // Odom verilerini güncelle
     robot_x_ = msg->pose.pose.position.x;
     robot_y_ = msg->pose.pose.position.y;
-  }
+}
+
+CostmapErasePlugin::~CostmapErasePlugin() {
+    if (inflation_layer_ != nullptr) {
+        delete inflation_layer_;
+        inflation_layer_ = nullptr;
+    }
+}
+
 } // end namespace costmap_2d
